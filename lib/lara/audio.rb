0ed2325db49a72a0a86cc73538408ea151b0d49e
Fabio Fioramonti <44729888+f4b1ux@users.noreply.github.com>
2026-08-25 10:53:16 +0200

    Add voice cloning option to audio translation

--- lib/lara/audio.rb
+++ lib/lara/audio.rb
@@ -25,7 +25,7 @@ module Lara
     # Uploads an audio file to S3 and creates a translation job.
     # @return [Lara::Models::Audio]
     def upload(file_path:, filename:, target:, source: nil, adapt_to: nil, glossaries: nil,
-               no_trace: false, style: nil, voice_gender: nil)
+               no_trace: false, style: nil, voice_gender: nil, voice_cloning: nil)
       response_data = @client.get("/v2/audio/upload-url", params: { filename: filename })
       url = response_data["url"]
       fields = response_data["fields"]
@@ -39,6 +39,7 @@ module Lara
         adapt_to: adapt_to,
         glossaries: glossaries,
         style: style,
+        voice_cloning: voice_cloning,
         voice_gender: voice_gender
       }.compact
 
@@ -68,10 +69,10 @@ module Lara
     # Translates an audio file end-to-end
     # @return [String] translated audio bytes
     def translate(file_path:, filename:, target:, source: nil, adapt_to: nil, glossaries: nil,
-                  no_trace: false, style: nil, voice_gender: nil)
+                  no_trace: false, style: nil, voice_gender: nil, voice_cloning: nil)
       audio = upload(file_path: file_path, filename: filename, target: target, source: source,
                      adapt_to: adapt_to, glossaries: glossaries, no_trace: no_trace, style: style,
-                     voice_gender: voice_gender)
+                     voice_gender: voice_gender, voice_cloning: voice_cloning)
 
       max_wait_time = 60 * 15 # 15 minutes
       start = Time.now
